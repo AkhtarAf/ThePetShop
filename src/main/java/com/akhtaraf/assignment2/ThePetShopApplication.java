cb8abfe126cbd1a44e4446a5339f14951d814122
Afshan Akhtar <akhtaraf@sheridancollege.ca>
2023-06-18 12:52:34 +0530

    init: Project Initiated with dependencies

---
+++ src/main/java/com/akhtaraf/assignment2/ThePetShopApplication.java
@@ -0,0 +1,13 @@
+package com.akhtaraf.assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThePetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akhtaraf/assignment2/ThePetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhtaraf.assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
